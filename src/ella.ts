aba36c1abaec40f8c131adcc574e994979247833
Lea Rosema <terabaud@gmail.com>
2020-05-10 16:19:09 +0200

    fix: add missing import

--- src/ella.ts
+++ src/ella.ts
@@ -2,5 +2,6 @@ import { Vec2, Vec3, Vec4 } from "./vector";
 import { Mat, Mat2, Mat3, Mat4 } from "./matrix";
 import { lookAt } from "./lookat";
 import { perspective, frustum, ortho } from "./perspective";
+import Geometry from './geometry';
 
-export { Vec2, Vec3, Vec4, Mat, Mat2, Mat3, Mat4, perspective, frustum, ortho, lookAt };
+export { Vec2, Vec3, Vec4, Mat, Mat2, Mat3, Mat4, Geometry, perspective, frustum, ortho, lookAt };
