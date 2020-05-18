0eb97abb534616abd9cb40f462cac09c302ff08e
Lea Rosema <terabaud@gmail.com>
2020-05-18 23:35:06 +0200

    fix: fix rollup UMD namespace

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,16 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/ella.ts',
-  output: [{
-    file: 'dist/ella.esm.js',
-    format: 'es'
-  }, {
-    file: 'dist/ella.umd.js',
-    format: 'umd',
-    name: 'GLea'
-  }],
-  plugins: [
-    typescript()
-  ]
+  output: [
+    {
+      file: 'dist/ella.esm.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/ella.umd.js',
+      format: 'umd',
+      name: 'Ella',
+    },
+  ],
+  plugins: [typescript()],
 };
