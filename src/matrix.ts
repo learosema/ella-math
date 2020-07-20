a1d7c65f15c3bb66fe067b85ca9ac4663e75b59a
Lea Rosema <terabaud@gmail.com>
2020-07-20 23:58:17 +0200

    feat: inverse matrix

--- src/matrix.ts
+++ src/matrix.ts
@@ -264,6 +264,113 @@ export class Mat {
   }
 
   /**
+   * calculate M^-1 for mat2, mat3, mat4
+   */
+  inverse() {
+    const { numRows, numCols } = this;
+    if (numRows !== numCols) {
+      throw Error('ArgumentError');
+    }
+    // glsl uses column major notation
+    const m = (col: number, row: number) => this.valueAt(row, col);
+
+    if (numRows === 2) {
+      const det = m(0, 0) * m(1, 1) - m(0, 1) * m(1, 0);
+      // prettier-ignore
+      return new Mat([
+         m(1, 1), -m(0, 1),
+        -m(1, 0),  m(0, 0)
+      ].map(x => x / det));
+    }
+    if (numRows === 3) {
+      const a00 = m(0, 0),
+        a01 = m(0, 1),
+        a02 = m(0, 2);
+      const a10 = m(1, 0),
+        a11 = m(1, 1),
+        a12 = m(1, 2);
+      const a20 = m(2, 0),
+        a21 = m(2, 1),
+        a22 = m(2, 2);
+
+      const b01 = a22 * a11 - a12 * a21;
+      const b11 = -a22 * a10 + a12 * a20;
+      const b21 = a21 * a10 - a11 * a20;
+
+      const det = a00 * b01 + a01 * b11 + a02 * b21;
+
+      return new Mat(
+        [
+          b01,
+          -a22 * a01 + a02 * a21,
+          a12 * a01 - a02 * a11,
+          b11,
+          a22 * a00 - a02 * a20,
+          -a12 * a00 + a02 * a10,
+          b21,
+          -a21 * a00 + a01 * a20,
+          a11 * a00 - a01 * a10,
+        ].map((x) => x / det)
+      );
+    }
+    if (numRows === 4) {
+      const a00 = m(0, 0),
+        a01 = m(0, 1),
+        a02 = m(0, 2),
+        a03 = m(0, 3);
+      const a10 = m(1, 0),
+        a11 = m(1, 1),
+        a12 = m(1, 2),
+        a13 = m(1, 3);
+      const a20 = m(2, 0),
+        a21 = m(2, 1),
+        a22 = m(2, 2),
+        a23 = m(2, 3);
+      const a30 = m(3, 0),
+        a31 = m(3, 1),
+        a32 = m(3, 2),
+        a33 = m(3, 3);
+
+      const b00 = a00 * a11 - a01 * a10;
+      const b01 = a00 * a12 - a02 * a10;
+      const b02 = a00 * a13 - a03 * a10;
+      const b03 = a01 * a12 - a02 * a11;
+      const b04 = a01 * a13 - a03 * a11;
+      const b05 = a02 * a13 - a03 * a12;
+      const b06 = a20 * a31 - a21 * a30;
+      const b07 = a20 * a32 - a22 * a30;
+      const b08 = a20 * a33 - a23 * a30;
+      const b09 = a21 * a32 - a22 * a31;
+      const b10 = a21 * a33 - a23 * a31;
+      const b11 = a22 * a33 - a23 * a32;
+
+      const det =
+        b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+      return new Mat(
+        [
+          a11 * b11 - a12 * b10 + a13 * b09,
+          a02 * b10 - a01 * b11 - a03 * b09,
+          a31 * b05 - a32 * b04 + a33 * b03,
+          a22 * b04 - a21 * b05 - a23 * b03,
+          a12 * b08 - a10 * b11 - a13 * b07,
+          a00 * b11 - a02 * b08 + a03 * b07,
+          a32 * b02 - a30 * b05 - a33 * b01,
+          a20 * b05 - a22 * b02 + a23 * b01,
+          a10 * b10 - a11 * b08 + a13 * b06,
+          a01 * b08 - a00 * b10 - a03 * b06,
+          a30 * b04 - a31 * b02 + a33 * b00,
+          a21 * b02 - a20 * b04 - a23 * b00,
+          a11 * b07 - a10 * b09 - a12 * b06,
+          a00 * b09 - a01 * b07 + a02 * b06,
+          a31 * b01 - a30 * b03 - a32 * b00,
+          a20 * b03 - a21 * b01 + a22 * b00,
+        ].map((x) => x / det)
+      );
+    }
+    throw Error('NotImplementedYet');
+  }
+
+  /**
    * convert matrix to string
    * @returns a string containing matROWSxCOLS(comma-separated-values)
    */
