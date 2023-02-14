d09d2af8a2aa90486e1e6177ef84bbe475305f7c
Lea Rosema <terabaud@gmail.com>
2023-02-14 22:47:33 +0000

    feat: add es5 support

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,30 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.esm.js',
-      format: 'es',
-    },
-    {
-      file: 'dist/index.umd.js',
-      format: 'umd',
-      name: 'Ella',
-    },
-  ],
-  plugins: [typescript()],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [typescript()],
+    output: [
+      {
+        file: 'dist/index.esm.js',
+        format: 'es',
+      },
+      {
+        file: 'dist/index.umd.js',
+        format: 'umd',
+        name: 'Ella',
+      },
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [typescript({ target: 'es5' })],
+    output: [
+      {
+        file: 'dist/index.umd.es5.js',
+        format: 'umd',
+        name: 'Ella',
+      },
+    ],
+  },
+];
